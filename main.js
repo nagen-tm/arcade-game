fb9e748cfdce47baaa88de19b829183753b088b6
Therese Nagengast <nagengtm@gmail.com>
2022-10-01 11:45:48 -0400

    fixed speed to have a max

--- main.js
+++ main.js
@@ -7,27 +7,21 @@ window.addEventListener('load', function(){
   const gameHeight = canvas.height = 500;
 
   /* objects:
-    each object has a constructor property that creates a new
-    object when the class is called. properties are then assigned
+    each object has a constructor property that creates a new object when the class is called. properties are then assigned
     based on the object's design
   */
   class InputHandler {
     constructor(game){
       this.game = game;
-      /* 
-      the input handler adds to an array the key press 
-      when key is down, removes it from the array when key is up 
-      */
+      // the input handler adds to an array the key press when key is down, removes it from the array when key is up 
       window.addEventListener('keydown', e => {
-        /* 
-        checks if arrow up is pressed and not already in the array
-        this stops holding the up key down continuously
-        */
+        // checks if arrow up is pressed and not already in the array this stops holding the up key down continuously
         if(((e.key === 'ArrowUp') || (e.key === 'ArrowDown')) && this.game.keys.indexOf(e.key) === -1) {
           this.game.keys.push(e.key);
         }
         console.log(this.game.keys);
       });
+
       window.addEventListener('keyup', e => {
         let keyIndex = this.game.keys.indexOf(e.key);
         if (keyIndex > -1){
@@ -55,11 +49,13 @@ window.addEventListener('load', function(){
       this.x = 20;
       this.y = 100;
       this.speedY = 0;
+      this.maxSpeed = 2;
     }
     update(){
       // updates player behavior from input
-      if (this.game.keys.includes('ArrowUp')) this.speedY = -1;
-      else if (this.game.keys.includes('ArrowDown')) this.speedY = 1;
+      if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+      else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+      else this.speedY = 0
       this.y += this.speedY;
     }
     draw(context){
@@ -104,9 +100,8 @@ window.addEventListener('load', function(){
   }
 
   /* 
-  calling the game object that in turn executes its constructor
-  and builds out the other objects. the new Game() needs to pass 
-  all relevant information
+  calling the game object that in turn executes its constructor and builds out the other objects. the new Game()
+  needs to pass all relevant information
   */
   const game = new Game(gameWidth, gameHeight);
 
@@ -116,10 +111,8 @@ window.addEventListener('load', function(){
     game.update();
     game.draw(ctx);
     /* 
-    tells browser we are making an animation and calls a 
-    specific function to update the animation
-    passing it the parent function creates and endless 
-    animation loop
+    tells browser we are making an animation and calls a specific function to update the animation passing it the 
+    parent function creates and endless animation loop
     */
     requestAnimationFrame(animation);
   }
