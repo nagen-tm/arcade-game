67c8e27c1e7d3cf467853e8ecccc8bc01737d681
Therese Nagengast <nagengtm@gmail.com>
2022-10-01 10:51:23 -0400

    update with basic player animation

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ window.addEventListener('load', function(){
   const canvas = document.getElementById('gameCanvas');
   // this sets 2D or 3D property
   const ctx = canvas.getContext('2d');
-  canvas.width = 1500;
-  canvas.height = 500;
+  const gameWidth = canvas.width = 1500;
+  const gameHeight = canvas.height = 500;
 
   /* objects:
     each object has a constructor property that creates a new
@@ -25,14 +25,14 @@ window.addEventListener('load', function(){
       this.game = game;
       this.width = 120;
       this.height= 190;
-      this.x =20;
-      this.y =100;
-      this.speedY = 0;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0.2;
     }
     update(){
       this.y += this.speedY;
     }
-    draw(){
+    draw(context){
       context.fillRect(this.x, this.y, this.width, this.height);
     }
   }
@@ -54,5 +54,34 @@ window.addEventListener('load', function(){
       this.height = height;
       this.player = new Player(this);
     }
+    update(){
+      this.player.update()
+    }
+    draw(context){
+      this.player.draw(context)
+    }
   }
+
+  /* 
+  calling the game object that in turn executes its constructor
+  and builds out the other objects. the new Game() needs to pass 
+  all relevant information
+  */
+  const game = new Game(gameWidth, gameHeight);
+
+  // animation loop
+  function animation(){
+    ctx.clearRect(0, 0, gameWidth, gameHeight);
+    game.update();
+    game.draw(ctx);
+    /* 
+    tells browser we are making an animation and calls a 
+    specific function to update the animation
+    passing it the parent function creates and endless 
+    animation loop
+    */
+    requestAnimationFrame(animation);
+  }
+
+  animation();
 });
