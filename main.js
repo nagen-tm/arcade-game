d0ca09badb57aff60cb4237e0eecc31477e9c8fa
Therese Nagengast <nagengtm@gmail.com>
2022-10-01 17:20:34 -0400

    added ammo and UI ammo use

--- main.js
+++ main.js
@@ -18,8 +18,9 @@ window.addEventListener('load', function(){
         // checks if arrow up is pressed and not already in the array this stops holding the up key down continuously
         if(((e.key === 'ArrowUp') || (e.key === 'ArrowDown')) && this.game.keys.indexOf(e.key) === -1) {
           this.game.keys.push(e.key);
+        } else if (e.key === ' '){
+          this.game.player.shoot();
         }
-        console.log(this.game.keys);
       });
 
       window.addEventListener('keyup', e => {
@@ -27,13 +28,27 @@ window.addEventListener('load', function(){
         if (keyIndex > -1){
           this.game.keys.splice(keyIndex, 1);
         }
-        console.log(this.game.keys);
       });
     }
   }
   class Projectile {
-    constructor(){
-      
+    constructor(game, x, y){
+      this.game = game;
+      this.x = x;
+      this.y =y;
+      this.width = 10;
+      this.height= 3;
+      this.speed = 3;
+      this.markedForDeletion = false;
+    }
+    update(){
+      // removes the laser after getting to 80% of the screen
+      this.x += this.speed;
+      if (this.x > this.game.width * 0.8) this.markedForDeletion = true;
+    }
+    draw(context){
+      context.fillStyle = 'yellow';
+      context.fillRect(this.x, this.y, this.width, this.height)
     }
   }
   class Particle {
@@ -50,6 +65,7 @@ window.addEventListener('load', function(){
       this.y = 100;
       this.speedY = 0;
       this.maxSpeed = 2;
+      this.projectiles= [];
     }
     update(){
       // updates player behavior from input
@@ -57,9 +73,26 @@ window.addEventListener('load', function(){
       else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
       else this.speedY = 0
       this.y += this.speedY;
+      // updated projectile and removes from array based on boolean
+      this.projectiles.forEach(projectile => {
+        projectile.update();
+      });
+      this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
     }
     draw(context){
+      context.fillStyle = 'black';
       context.fillRect(this.x, this.y, this.width, this.height);
+      this.projectiles.forEach(projectile => {
+        projectile.draw(context);
+      });
+    }
+    shoot(){
+      // creates projectile based on ammo amount 
+      if (this.game.ammo > 0){
+        this.projectiles.push(new Projectile(this.game, this.x, this.y));
+        console.log(this.projectiles);
+        this.game.ammo--;
+      }
     }
   }
   class Enemy {
@@ -78,24 +111,46 @@ window.addEventListener('load', function(){
     }
   }
   class UI {
-    constructor(){
-      
+    constructor(game){
+      this.game = game;
+      this.fontSize = 25;
+      this.fontFamily = 'Helvetica';
+      this.color = 'white';
+    }
+    draw(context){
+      context.fillStyle = this.color;
+      for(let i = 0; i < this.game.ammo; i++){
+        context.fillRect(20+5 * i, 50, 3, 20)
+      }
     }
   }
   class Game {
+    // builds all objects from the classes for the game
     constructor(width, height){
       this.width = width;
       this.height = height;
       // passing this to the new objects refers to this Game class
       this.player = new Player(this);
       this.input = new InputHandler(this);
+      this.ui = new UI(this);
       this.keys = [];
+      this.ammo = 20;
+      this.maxAmmo = 50;
+      this.ammoTimer = 0;
+      this.ammoInterval = 500;
     }
-    update(){
+    update(deltaTime){
       this.player.update()
+      if (this.ammoTimer > this.ammoInterval){
+        if (this.ammo < this.maxAmmo) this.ammo++
+        this.ammoTimer = 0;
+      } else {
+        this.ammoTimer += deltaTime;
+      }
     }
     draw(context){
       this.player.draw(context)
+      this.ui.draw(context);
     }
   }
 
@@ -104,11 +159,16 @@ window.addEventListener('load', function(){
   needs to pass all relevant information
   */
   const game = new Game(gameWidth, gameHeight);
+  let lastTime = 0;
 
   // animation loop
-  function animation(){
+  function animation(timeStamp){
+    // to calculate the time for regenerating ammo, deltaTime compares current time stamp to last time stamp
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
     ctx.clearRect(0, 0, gameWidth, gameHeight);
-    game.update();
+    game.update(deltaTime);
     game.draw(ctx);
     /* 
     tells browser we are making an animation and calls a specific function to update the animation passing it the 
@@ -117,5 +177,5 @@ window.addEventListener('load', function(){
     requestAnimationFrame(animation);
   }
 
-  animation();
+  animation(0);
 });
